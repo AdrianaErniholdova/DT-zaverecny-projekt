eb54786046a111f5acbb3c39f02c3c627fe3cb66
Adela Máčajová <adela.macajova3@gmail.com>
2026-01-02 18:59:53 +0100

    pridanie komentárov do sql

--- sql/transform.sql
+++ sql/transform.sql
@@ -1,4 +1,4 @@
-//dimenzie
+--Vytvorenie dimenzie item
 CREATE OR REPLACE TABLE dim_item AS (
 SELECT DISTINCT
     i.SPS_ITEM_MAPPING_KEY AS id_dim_item,
@@ -13,6 +13,7 @@ FROM item_staging i
 
 SELECT * FROM dim_item LIMIT 10;
 
+--Vytvorenie dimenzie location
 CREATE OR REPLACE TABLE dim_location AS (
 SELECT DISTINCT
     l.SPS_CUSTOMER_LOCATION_KEY AS id_dim_location,
@@ -25,6 +26,7 @@ FROM location_staging l
 
 SELECT * FROM dim_location LIMIT 10;
 
+--Vytvorenie dimenzie retailer
 CREATE OR REPLACE TABLE dim_retailer AS (
 SELECT DISTINCT
     r.SPS_RETAILER_NAME_KEY AS id_dim_retailer,
@@ -34,18 +36,20 @@ FROM retailer_staging r
 
 SELECT * FROM dim_retailer LIMIT 10;
 
+--Vytvorenie dimenzie store
 CREATE OR REPLACE TABLE dim_store AS (
 SELECT DISTINCT
-  l.SPS_CUSTOMER_LOCATION_KEY AS id_dim_store,
-  CAST(l.STORE_NAME AS VARCHAR(50)) AS store_name,
-  CAST(l.STORE_NUMBER AS VARCHAR(10)) AS store_number,
-  CAST(l.SPS_LOCATION_TYPE AS VARCHAR(8)) AS store_type,
-  CAST(l.MALL AS VARCHAR(30)) AS mall_type
+    l.SPS_CUSTOMER_LOCATION_KEY AS id_dim_store,
+    CAST(l.STORE_NAME AS VARCHAR(50)) AS store_name,
+    CAST(l.STORE_NUMBER AS VARCHAR(10)) AS store_number,
+    CAST(l.SPS_LOCATION_TYPE AS VARCHAR(8)) AS store_type,
+    CAST(l.MALL AS VARCHAR(30)) AS mall_type
 FROM location_staging l    
 );
 
 SELECT * FROM dim_store LIMIT 10;
 
+--Vytvorenie dimenzie date
 CREATE OR REPLACE TABLE dim_date AS (
 SELECT DISTINCT
     a.PERIOD_ENDING_DATE AS id_dim_date,
@@ -61,21 +65,22 @@ FROM activity_staging a
 
 SELECT * FROM dim_date LIMIT 10;
 
+--Vytvorenie faktovej tabuľky
 CREATE OR REPLACE TABLE fact_sales AS (
 SELECT
-   UUID_STRING() AS id_fact_sales,
-   dr.id_dim_retailer,
-   di.id_dim_item,
-   dl.id_dim_location,
-   dd.id_dim_date,
-   ds.id_dim_store,
-   a.NET_SALES_UNITS AS net_sales_units,
-   CAST(a.NET_SALES_RETAIL AS NUMBER(8,4)) AS net_sales_retail,
-   a.INVENTORY_UNITS AS inventory_units,
-   a.CORPORATE_UNIT_OWNED_RETAIL_PRICE AS retail_price,
-   a.CORPORATE_UNIT_ADJUSTED_COST AS corporate_cost,
-   SUM(a.net_sales_units) OVER (PARTITION BY a.sps_item_mapping_key , a.sps_customer_location_key ORDER BY a.period_ending_date) AS cumulative_sales,
-   LAG(a.net_sales_units) OVER (PARTITION BY a.sps_item_mapping_key , a.sps_customer_location_key ORDER BY a.period_ending_date) AS prev_weekly_sales 
+    UUID_STRING() AS id_fact_sales,
+    dr.id_dim_retailer,
+    di.id_dim_item,
+    dl.id_dim_location,
+    dd.id_dim_date,
+    ds.id_dim_store,
+    a.NET_SALES_UNITS AS net_sales_units,
+    CAST(a.NET_SALES_RETAIL AS NUMBER(8,4)) AS net_sales_retail,
+    a.INVENTORY_UNITS AS inventory_units,
+    a.CORPORATE_UNIT_OWNED_RETAIL_PRICE AS retail_price,
+    a.CORPORATE_UNIT_ADJUSTED_COST AS corporate_cost,
+    SUM(a.net_sales_units) OVER (PARTITION BY a.sps_item_mapping_key , a.sps_customer_location_key ORDER BY a.period_ending_date) AS cumulative_sales,
+    LAG(a.net_sales_units) OVER (PARTITION BY a.sps_item_mapping_key , a.sps_customer_location_key ORDER BY a.period_ending_date) AS prev_weekly_sales 
 FROM activity_staging a 
 JOIN dim_retailer dr ON a.SPS_RETAILER_NAME_KEY = dr.id_dim_retailer
 JOIN dim_item di ON a.SPS_ITEM_MAPPING_KEY = di.id_dim_item
